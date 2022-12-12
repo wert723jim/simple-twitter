71f8b5ab7faccef89356cd65440bb136530f5831
wert723jim <wert723jim@gmail.com>
2022-12-12 17:50:09 +0800

    Adding Followings page & Followers page

---
+++ src/components/FollowTab.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="tab-container">
+    <div class="tab-item">
+      <router-link
+        :to="{name: 'user-followers', params: {id: 1}}" 
+        class="tab-link"
+      >
+        追隨者
+      </router-link>
+    </div>
+    <div class="tab-item">
+      <router-link
+        :to="{name: 'user-followings', params: {id: 1}}"
+        class="tab-link"
+      >
+        追隨中
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+  .tab-container {
+    display: flex;
+    border-bottom: 1px solid #E6ECF0;
+  }
+
+  .tab-item {
+    display: flex;
+    height: 52px;
+    width: 130px;
+  }
+
+  .tab-link {
+    width: 100%;
+    height: 100%;
+    line-height: 50px;
+    color: #657786;
+  }
+
+  .active {
+    border-bottom: 2px solid #FF6600;
+    color: #FF6600;
+  }
+</style>
---
+++ src/components/FollowingsList.vue
@@ -0,0 +1,152 @@
+<template>
+  <div class="followings-container">
+    <div class="following-wrapper">
+      <div class="wrapper-left">
+        <div class="head-shot"></div>
+      </div>
+      <div class="wrapper-right">
+        <div class="right-header">
+          <div class="name">
+            Banana
+          </div>
+          <div class="button-wrapper">
+            <button class="follow-btn">
+              跟隨
+            </button>
+          </div>
+        </div>
+        <div class="intro">
+          Nulla Lorem mollit cupidatat irure. Laborum magna null duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum.
+        </div>
+      </div>
+    </div>
+
+    <div class="following-wrapper">
+      <div class="wrapper-left">
+        <div class="head-shot"></div>
+      </div>
+      <div class="wrapper-right">
+        <div class="right-header">
+          <div class="name">
+            Banana
+          </div>
+          <div class="button-wrapper">
+            <button class="follow-btn">
+              跟隨
+            </button>
+          </div>
+        </div>
+        <div class="intro">
+          Nulla Lorem mollit cupidatat irure. Laborum magna null duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum.
+        </div>
+      </div>
+    </div>
+
+    <div class="following-wrapper">
+      <div class="wrapper-left">
+        <div class="head-shot"></div>
+      </div>
+      <div class="wrapper-right">
+        <div class="right-header">
+          <div class="name">
+            Banana
+          </div>
+          <div class="button-wrapper">
+            <button class="follow-btn">
+              跟隨
+            </button>
+          </div>
+        </div>
+        <div class="intro">
+          Nulla Lorem mollit cupidatat irure. Laborum magna null duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum.
+        </div>
+      </div>
+    </div>
+
+    <div class="following-wrapper">
+      <div class="wrapper-left">
+        <div class="head-shot"></div>
+      </div>
+      <div class="wrapper-right">
+        <div class="right-header">
+          <div class="name">
+            Banana
+          </div>
+          <div class="button-wrapper">
+            <button class="follow-btn">
+              跟隨
+            </button>
+          </div>
+        </div>
+        <div class="intro">
+          Nulla Lorem mollit cupidatat irure. Laborum magna null duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum.
+        </div>
+      </div>
+    </div>
+
+    <div class="following-wrapper">
+      <div class="wrapper-left">
+        <div class="head-shot"></div>
+      </div>
+      <div class="wrapper-right">
+        <div class="right-header">
+          <div class="name">
+            Banana
+          </div>
+          <div class="button-wrapper">
+            <button class="follow-btn">
+              跟隨
+            </button>
+          </div>
+        </div>
+        <div class="intro">
+          Nulla Lorem mollit cupidatat irure. Laborum magna null duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum.
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .following-wrapper {
+    text-align: left;
+    padding: 16px 23px;
+    display: flex;
+    min-height: 158px;
+    border-bottom: 1px solid #E6ECF0;
+  }
+
+  .head-shot {
+    height: 50px;
+    width: 50px;
+    border-radius: 50px;
+    background: gray;
+  }
+
+  .wrapper-right {
+    padding: 8px;
+  }
+
+  .right-header {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 8px;
+  }
+
+  /* .name {
+    line-height: 40px;
+  } */
+
+  .follow-btn {
+    width: 64px;
+    height: 40px;
+    border: 1px solid #FF6600;
+    border-radius: 50px;
+    color: #FF6600;
+    background: #FFFFFF;
+  }
+
+  .intro {
+    font-size: 16px;
+  }
+</style>
